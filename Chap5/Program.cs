9324b24b780ad5711c00729842945ca99f92a2aa
[MarcusQ] <marcusquigley@gmail.com>
2019-02-03 18:58:11 -0500

    Interval emits

--- Chap5/Program.cs
+++ Chap5/Program.cs
@@ -5,22 +5,47 @@ using System.Linq;
 using System.Reactive.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Timers;
 
 namespace Chap5
 {
     class Program
     {
+        static Timer timer;
         [STAThread]
-        public static void Main(string[] args) {
+        public static void Main(string[] args)
+        {
+            SetupTimer();
 
             //TestPrime();
             // TestPrimes(10);
             //TestPrimesAsync(10);
 
-            TestPrimesObservable(10);
+            //  TestPrimesObservable(10);
+            TestPeriodics();
 
             Console.WriteLine("Something else");
             Console.ReadKey();
+        }
+
+        private static void SetupTimer()
+        {
+            timer = new Timer();
+            timer.Interval = 10000;
+            timer.Elapsed += Timer_Elapsed;
+            timer.Start();
+        }
+
+        private static void Timer_Elapsed(object sender, ElapsedEventArgs e)
+        {
+            timer.Stop();
+            timer.Dispose();
+
+            if (disposable != null)
+            {
+                disposable.Dispose();
+                Console.WriteLine("Periodic service call ended");
+            }
         }
 
         static void TestPrimes(int number)
@@ -59,6 +84,18 @@ namespace Chap5
                                 //.GeneratePrimesObs(number)
                                  .Timestamp()
                                  .SubscribeConsole("primes observable");
+
+        }
+
+        static IDisposable disposable;
+
+        static void TestPeriodics()
+        {
+            IService service = new SomeService();
+            PeriodicBehaviour pb = new PeriodicBehaviour(service);
+            //disposable =  pb.GetIntervalData();
+
+            pb.GetTimerIntervalData();
 
         }
 
